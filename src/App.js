d395e7faa02afbb8031d5cde39feceb53133f5c4
RonnieGurr <ronniegurr12@gmail.com>
2020-10-05 00:43:53 +0100

    Footer removed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import Todos from './components/Todos';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
     <Header />
     <Todos />
-    <Footer />
     </div>
   );
 }
